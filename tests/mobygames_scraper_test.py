40e3e6f1ac0e5d1e7fdd203d4158af42a1c50b19
Christian Jungerius <cjungerius@olbico.nl>
2021-10-18 17:31:58 +0200

    fixing tests

--- tests/mobygames_scraper_test.py
+++ tests/mobygames_scraper_test.py
@@ -1,15 +1,23 @@
-import unittest, mock, os, sys, re
+import unittest, os
+import unittest.mock
+from unittest.mock import MagicMock, patch
 
-from mock import *
-from mock import ANY
-from tests.fakes import *
-import xml.etree.ElementTree as ET
+import json
+import re
+import logging
 
-from resources.utils import *
-from resources.net_IO import *
-from resources.scrap import *
-from resources.objects import *
-from resources.constants import *
+from fakes import FakeProgressDialog, random_string
+
+logging.basicConfig(format = '%(asctime)s %(module)s %(levelname)s: %(message)s',
+                datefmt = '%m/%d/%Y %I:%M:%S %p', level = logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+from resources.lib.scraper import MobyGames
+from ael.scrapers import ScrapeStrategy, ScraperSettings
+
+from ael.api import ROMObj
+from ael import constants
+from ael.utils import net
         
 def read_file(path):
     with open(path, 'r') as f:
@@ -19,7 +27,7 @@ def read_file_as_json(path):
     file_data = read_file(path)
     return json.loads(file_data, encoding = 'utf-8')
 
-def mocked_gamesdb(url, url_log=None):
+def mocked_mobygames(url, url_log=None):
 
     mocked_json_file = ''
 
@@ -36,7 +44,7 @@ def mocked_gamesdb(url, url_log=None):
         mocked_json_file = Test_mobygames_scraper.TEST_ASSETS_DIR + "\\mobygames_castlevania.json"
         
     if mocked_json_file == '':
-        return net_get_URL(url)
+        return net.get_URL(url)
 
     print('reading mocked data from file: {}'.format(mocked_json_file))
     return read_file(mocked_json_file), 200
@@ -48,9 +56,7 @@ class Test_mobygames_scraper(unittest.TestCase):
     TEST_ASSETS_DIR = ''
 
     @classmethod
-    def setUpClass(cls):
-        set_log_level(LOG_DEBUG)
-        
+    def setUpClass(cls):        
         cls.TEST_DIR = os.path.dirname(os.path.abspath(__file__))
         cls.ROOT_DIR = os.path.abspath(os.path.join(cls.TEST_DIR, os.pardir))
         cls.TEST_ASSETS_DIR = os.path.abspath(os.path.join(cls.TEST_DIR,'assets/'))
@@ -60,64 +66,63 @@ class Test_mobygames_scraper(unittest.TestCase):
         print('TEST ASSETS DIR: {}'.format(cls.TEST_ASSETS_DIR))
         print('---------------------------------------------------------------------------')
 
-    def get_test_settings(self):
-        settings = {}
-        settings['scan_metadata_policy'] = 3 # OnlineScraper only
-        settings['scan_asset_policy'] = 0
-        settings['metadata_scraper_mode'] = 1
-        settings['asset_scraper_mode'] = 1
-        settings['scan_clean_tags'] = True
-        settings['scan_ignore_scrap_title'] = False
-        settings['scraper_metadata'] = 0 # NullScraper
-        settings['scraper_mobygames_apikey'] = 'abc123'
-        settings['escape_romfile'] = False
-
-        return settings
-
-    @patch('resources.scrap.net_get_URL', side_effect = mocked_gamesdb)
-    def test_scraping_metadata_for_game(self, mock_json_downloader):
-        
+    @patch('resources.lib.scraper.net.get_URL', side_effect = mocked_mobygames)
+    @patch('ael.api.client_get_rom')
+    def test_scraping_metadata_for_game(self, api_rom_mock: MagicMock, mock_get):        
         # arrange
-        settings = self.get_test_settings()
-        status_dic = {}
-        status_dic['status'] = True
-        target = MobyGames(settings)
+        settings = ScraperSettings()
+        settings.scrape_metadata_policy = constants.SCRAPE_POLICY_SCRAPE_ONLY
+        settings.scrape_assets_policy = constants.SCRAPE_ACTION_NONE
+        
+        rom_id = random_string(5)
+        rom = ROMObj({
+            'id': rom_id,
+            'filename': Test_mobygames_scraper.TEST_ASSETS_DIR + '\\castlevania.zip',
+            'platform': 'Nintendo NES'
+        })
+        api_rom_mock.return_value = rom
+        
+        target = ScrapeStrategy(None, 0, settings, MobyGames(), FakeProgressDialog())
 
         # act
-        candidates = target.get_candidates('castlevania', 'castlevania', 'Nintendo NES', status_dic)
-        actual = target.get_metadata(candidates[0], status_dic)
-                          
+        actual = target.process_single_rom(rom_id)
+        
         # assert
         self.assertTrue(actual)
-        self.assertEqual(u'Castlevania', actual['title'])
-        print(actual)
-
-        
-    # add actual mobygames apikey above and comment out patch attributes to do live tests
-    @patch('resources.scrap.net_get_URL', side_effect = mocked_gamesdb)
-    @patch('resources.scrap.net_download_img')
-    def test_scraping_assets_for_game(self, mock_img_downloader, mock_json_downloader):
-
-        # arrange
-        settings = self.get_test_settings()
-        status_dic = {}
-        status_dic['status'] = True
-        target = MobyGames(settings)
-        
-        assets_to_scrape = [
-            g_assetFactory.get_asset_info(ASSET_BOXFRONT_ID), 
-            g_assetFactory.get_asset_info(ASSET_BOXBACK_ID), 
-            g_assetFactory.get_asset_info(ASSET_SNAP_ID)]
-        
-        # act
-        actuals = []
-        candidates = target.get_candidates('castlevania', 'castlevania', 'Nintendo NES', status_dic)   
-        for asset_to_scrape in assets_to_scrape:
-            an_actual = target.get_assets(candidates[0], asset_to_scrape, status_dic)
-            actuals.append(an_actual)
+        self.assertEqual(u'Castlevania', actual.get_name())
+        print(actual.get_data_dic())
                 
-        # assert
-        for actual in actuals:
-            self.assertTrue(actual)
+    # add actual mobygames apikey above and comment out patch attributes to do live tests
+    @patch('resources.lib.scraper.net.get_URL', side_effect = mocked_gamesfaq)
+    @patch('resources.lib.scraper.net.download_img')
+    @patch('ael.api.client_get_rom')
+    def test_scraping_assets_for_game(self, api_rom_mock: MagicMock, mock_imgs, mock_get):
+        # arrange
+        settings = ScraperSettings()
+        settings.scrape_metadata_policy = constants.SCRAPE_ACTION_NONE
+        settings.scrape_assets_policy = constants.SCRAPE_POLICY_SCRAPE_ONLY
+        settings.asset_IDs_to_scrape = [constants.ASSET_BOXFRONT_ID, constants.ASSET_BOXBACK_ID, constants.ASSET_SNAP_ID ]
         
-        print(actuals)
+        rom_id = random_string(5)
+        rom = ROMObj({
+            'id': rom_id,
+            'filename': Test_mobygames_scraper.TEST_ASSETS_DIR + '\\castlevania.zip',
+            'platform': 'Nintendo NES'
+        })
+        api_rom_mock.return_value = rom
+        
+        target = ScrapeStrategy(None, 0, settings, MobyGames(), FakeProgressDialog())
+
+        # act
+        actual = target.process_single_rom(rom_id)
+
+        # assert
+        self.assertTrue(actual)     
+        
+        actual_data = actual.get_data_dic()
+        actual_assets = actual_data['assets']
+                
+        self.assertTrue(actual_assets)     
+        #self.assertEqual(100, len(actual_assets))
+        for actual_key, actual_value in actual_assets.items():
+            logger.info('{} = {}'.format(actual_key, actual_value))
